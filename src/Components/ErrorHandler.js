68c05365647f1369668e9ba64b8207ec58f7fac1
Ranjeet Bansode <77234540+Wxld@users.noreply.github.com>
2022-07-29 00:22:51 +0530

    changed error page msg

--- src/Components/ErrorHandler.js
+++ src/Components/ErrorHandler.js
@@ -4,7 +4,7 @@ const ErrorHandler = ({error}) => {
   return (
     <div className='w-full h-[75vh] m-0 p-0 flex flex-col items-center'>
       <img className='mt-4 p-0 h-5/6' src={require("../asset/Search-rafiki.svg").default} alt="" />
-      <p className='text-xl text-gray-800'>Oops! {error}.</p>
+      <p className='text-xl text-gray-800'>Snap! {error}.</p>
     </div>
   )
 }
